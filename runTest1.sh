3bb7cf091af9797ac778e45ef382804da21acfa0
Michelle Reed <m.thompson@bham.ac.uk>
2017-06-29 16:29:25 +0100

    Update runTest1.sh

--- runTest1.sh
+++ runTest1.sh
@@ -27,20 +27,26 @@ echo "Installation of MATLAB Runtime and non-gui metabolab probably OK" > instal
 echo "hello, hello, hello world!"
 
 if cmp -s s_NEWST123.txt outputted_files/s_NEWST123.txt; then
+    echo "s files have the same content"
     echo "s files have the same content" > output_check
 else
+    echo "s files do NOT have the same content"
     echo "s files do NOT have the same content" > output_check
 fi
 
 
 if cmp -s a_NEWST123_metabolite_profiling_NMR_spectroscopy.txt outputted_files/a_NEWST123_metabolite_profiling_NMR_spectroscopy.txt; then
+    echo "a_*.txt isatab assay files identical"
     echo "a_*.txt isatab assay files identical" >> output_check
 else
+    echo "a_*.txt isatab assay files not identical - a_.txt file problem" >
     echo "a_*.txt isatab assay files not identical - a_.txt file problem" >> output_check
 fi
 
 if cmp -s MTBLS1output_batman.csv outputted_files/MTBLS1output_batman.csv; then
+    echo "batman files have the same content"
     echo "batman files have the same content" >> output_check
 else
+    echo "batman files do NOT have the same content" 
     echo "batman files do NOT have the same content" >> output_check
 fi
