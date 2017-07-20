8f6ce0e9f40c170458bcc06574fe90c71d255540
Michelle A.C. Reed <m.thompson@bham.ac.uk>
2017-07-20 09:48:38 +0100

    modifying runMetabolab.sh to be OK when no data

--- runMetabolab.sh
+++ runMetabolab.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#!/bin/bash
+if [ -e container_script.ml ]
+then
+   echo "OK"
+   /usr/local/bin/metabolab1d/run_MetaboLab1D.sh /usr/local/bin/MATLAB_Runtime/v91 /mydata/container_script.ml
+else 
+   echo "add in script"
+fi
 
-/usr/local/bin/metabolab1d/run_MetaboLab1D.sh /usr/local/bin/MATLAB_Runtime/v91 /mydata/container_script.ml
 
